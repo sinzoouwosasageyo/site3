116f264cf8633d83299c7fce07e30f011b1e9076
sinzoouwosasageyo <limshout67@gmail.com>
2025-01-09 12:53:42 +0900

    디자인투플라이 3일차

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+/*header 스크롤클래스 호출*/
+var header = new EzenScrollClass("header",{
+  baseline: 100,
+});
+
 /*main_visual 스와이퍼 호출*/
 var main_visual = new Swiper("#main_visual",{
   wrapperClass:"slider",  //슬라이드 요소를 감싸는 요소의 클래스
@@ -5,8 +10,9 @@ var main_visual = new Swiper("#main_visual",{
   slideActiveClass:"active",    //보이는 슬라이드에 추가되는 클래스명
   spaceBetween: 10,   //사이간격
   speed: 800,   //슬라이드 되는 시간
+  loop: true,
   autoplay:{    //자동슬라이드
-    delay: 5000,    //전환간격(5초)
+    delay: 5000,    //전환간격(5초)    
   },
   pagination: {   //페이지버튼 생성
     el: '.pager',   //버튼영역 지정
